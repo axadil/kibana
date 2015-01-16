17eb0d7a55a16298c3db892c895fb9fe6eb97899
Jean-Marc Leoni <jeanmarc.leoni@axa.com>
2015-01-16 11:32:01 +0100

    WIP adding not specified bucket in Term Aggs need to fix tests

--- src/kibana/components/agg_response/hierarchical/build_hierarchical_data.js
+++ src/kibana/components/agg_response/hierarchical/build_hierarchical_data.js
@@ -60,6 +60,20 @@ define(function (require) {
         var split = buildSplit(firstAgg, metric, aggData);
         split.hits = resp.hits.total;
         split.raw = raw;
+        // TODO : add the option to have a not specified slice in the GUI
+        if (firstAgg.type.name === 'terms') {
+          var specified = _.reduce(raw.rows, function (memo, num) { return memo + num[1]; }, 0);
+          var notSpecifiedName = 'Not Specified';
+          var notSpecifiedCount = split.hits - specified;
+           if (notSpecifiedCount > 0){
+            split.raw.rows = _.union(raw.rows, [[notSpecifiedName, notSpecifiedCount]]);
+            split.names = _.union(split.names, [notSpecifiedName]);
+            split.slices.children = _.union(
+                split.slices.children, [{'name': notSpecifiedName,
+                'size': notSpecifiedCount,
+                'aggConfig': split.slices.children[0].aggConfig}]);
+          }
+        }
         split.tooltipFormatter = tooltipFormatter(raw.columns);
         return split;
       }
