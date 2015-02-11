ccbaec46d6375eb7bd4b6d7e93dec7ae2f4cc461
Jean-Marc Leoni <jeanmarc.leoni@axa.com>
2015-02-11 16:37:58 +0100

    Fix s3 upload bucket

--- tasks/config/s3.js
+++ tasks/config/s3.js
@@ -1,17 +1,17 @@
 module.exports = function (config) {
   return {
     release: {
-      bucket: 'download.elasticsearch.org',
+      bucket: 'tex-artifacts',
       access: 'private',
       //debug: true, // uncommment to prevent actual upload
       upload: [
         {
           src: 'target/<%= pkg.name %>-<%= pkg.version %>.zip',
-          dest: 'kibana/kibana/<%= pkg.name %>-<%= pkg.version %>.zip',
+          dest: 'kibana4/<%= pkg.name %>-<%= pkg.version %>.zip',
         },
         {
           src: 'target/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
-          dest: 'kibana/kibana/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
+          dest: 'kibana4/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
         }
       ]
     }
